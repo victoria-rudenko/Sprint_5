48cc9498a5271d89217a97a531bc780b2c24687d
Viktoria Rudenko <viktoria1.rudenko@yandex.ru>
2026-07-20 10:08:08 +0400

    Sprint_5: Fixed locators

--- locators.py
+++ locators.py
@@ -15,11 +15,11 @@ INPUT_EMAIL_REGISTRATION = (By.NAME, "email")
 INPUT_PASSWORD_REGISTRATION = (By.NAME, "password")
 INPUT_SUBMIT_PASSWORD_REGISTRATION = (By.NAME, "submitPassword")
 BUTTON_CREATE_ACCOUNT = (By.XPATH, "//button[@type='submit' and contains(., 'Создать')]")
-ERROR_MESSAGE = (By.XPATH, "//*[@id='root']/div/div[2]/div[5]/form/div[2]/div[1]/span")
+ERROR_MESSAGE = (By.XPATH, "//span[text()='Ошибка']")
 
 # После успешной регистрации
-ELEMENT_AVATAR_USER = (By.XPATH, "//*[@id='root']/div/div[1]/div/div[1]/button")
-ELEMENT_USERNAME_DISPLAY = (By.XPATH, "//*[@id='root']/div/div[1]/div/div[1]/div/h3")
+ELEMENT_AVATAR_USER = (By.XPATH, "//svg[@class='svgSmall']")
+ELEMENT_USERNAME_DISPLAY = (By.XPATH, "//h3[@class='profileText name']")
 BUTTON_LOGOUT = (By.XPATH, "//button[contains(text(), 'Выйти')]")
 AD_TITLE = (By.XPATH, "//h2[text()='Преступление и Наказание']")
 
